79fe4416665e82ce23eb66d8eac062e90782fcec
AAncapo <98633424+AAncapo@users.noreply.github.com>
2024-04-06 13:25:04 -0400

    Change camera settings

--- camera.gd
+++ camera.gd
@@ -14,24 +14,12 @@ var mouse_sensitivity: float = 0.05
 var rotation_speed: float = 0.2
 var isset_tactical:bool = false
 
-var cam_settings = {
-	'projection': [0, 1],
-	'size': [1, 50],
-	'parent/global_rotation_degrees/x': [-60, -90]
-	}
-
 
 func _ready() -> void:
-	set_tactical_mode(false)
 	GameEvents.focus_world_object.connect(on_world_object_focused)
-
 
 func _process(delta: float) -> void:
 	calc_move(delta)
-	
-	if Input.is_action_just_pressed("tactical_mode"): # T
-		set_tactical_mode(!isset_tactical)
-
 
 func _unhandled_input(event: InputEvent) -> void:
 	## Zoom camera w mouse wheel ##
@@ -52,7 +40,6 @@ func _unhandled_input(event: InputEvent) -> void:
 #			new_rotation_y = deg_to_rad(event.relative.x) * rotation_speed
 #			self.global_rotate(Vector3.UP, new_rotation_y)
 
-
 func calc_move(delta:float) -> void:
 	## Move camera using WASD/arrow keys ##
 	if Input.is_action_pressed("cam_left"):
@@ -66,14 +53,6 @@ func calc_move(delta:float) -> void:
 	
 	global_position = lerp(global_position,new_pos, delta * move_speed)
 	cam.position.z = lerp(cam.position.z, new_zoom, delta * move_speed)
-
-
-func set_tactical_mode(tactical:bool):
-	var index = 0 if !tactical else 1
-	for prop in cam_settings.keys():
-		cam.set(prop, cam_settings[prop][index])
-	isset_tactical = tactical
-
  
 func on_world_object_focused(obj, focus=true):
 	if focus:
